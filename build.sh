b8c7182ac5563de9d95b63da7e1ee65348ed9130
sai krishna <saikrishna.ogubina@cyberark.com>
2022-02-04 08:25:01 +0530

    Installing Maven Java

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 set -euo pipefail
 
-docker build . --build-arg java_version=11
+docker build . --build-arg maven_version=3.8.4 --build-arg java_version=11
+#FROM maven:${maven_version}-openjdk-${java_version}-slim
