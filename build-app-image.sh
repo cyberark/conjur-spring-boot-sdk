5bda1923b2582203173b5ff98187d26aa73d9cbf
Hugh Saunders <hugh.saunders@cyberark.com>
2022-02-08 11:54:44 +0000

    Add Jar Promotion Support
    * Ensure that Jars are appropriately versioned and pushed
      to the releases artifactory repo.
    * Ensure that Github releases also have release versioned Jars.
    
    Related: conjurinc/ops#861

--- build-app-image.sh
+++ build-app-image.sh
@@ -2,28 +2,23 @@
 
 set -euo pipefail
 
-if [[ -n "${BUILD_NUMBER:-}" ]]; then
-  tag="$(<VERSION)"
-else
-  tag="development"
-fi
-
-find target/*.jar || {
+if ! find target/*.jar; then
     echo "No jar found in target directory, please run build-package.sh before build-app-image.sh"
     exit 1
-}
+fi
 
-# Build tools image
-#FROM maven:${maven_version}-openjdk-${java_version}-slim
+if [[ ! -r VERSION ]]; then
+  echo "VERSION file not present, please run build-package.sh before build-app-image.sh"
+  exit 1
+fi
+
 docker build \
     --build-arg maven_version=3.8.4 \
     --build-arg java_version=11 \
     -t conjur-spring-boot-plugin \
-    -t "registry.tld/conjur-spring-boot-plugin:${tag}" \
+    -t "registry.tld/conjur-spring-boot-plugin:$(<VERSION)" \
     .
 
-
-#docker tag conjur-spring-boot-plugin registry.tld/conjur-spring-boot-plugin
 if [[ -n "${BUILD_NUMBER:-}" ]]; then
-  docker push  "registry.tld/conjur-spring-boot-plugin:${tag}" 
+  docker push  "registry.tld/conjur-spring-boot-plugin:$(<VERSION)"
 fi
