63d19b311ad36aff3cae7c455453c98fc0d125c2
Nitin Maheshwari <98587587+nitincyberark@users.noreply.github.com>
2023-04-13 16:16:26 +0530

    modified the code as per changes made by badr

--- src/main/java/com/cyberark/conjur/springboot/processor/ConjurValueProcessor.java
+++ src/main/java/com/cyberark/conjur/springboot/processor/ConjurValueProcessor.java
@@ -23,7 +23,6 @@ import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.Environment;
 
 import com.cyberark.conjur.springboot.constant.ConjurConstant;
-import com.cyberark.conjur.springboot.core.env.ConjurPropertySource;
 import com.cyberark.conjur.springboot.domain.ConjurProperties;
 import com.cyberark.conjur.springboot.service.CustomPropertySourceChain;
 import com.cyberark.conjur.springboot.service.DefaultPropertySourceChain;
@@ -69,7 +68,6 @@ public class ConjurValueProcessor implements BeanPostProcessor, InitializingBean
 		{
 
 		LOGGER.info("Start : afterPropertiesSet" + conjurProperties);}
-		initializeSysEnv(conjurProperties);
 		
 		this.processorChain = new DefaultPropertySourceChain("DefaultPropertySource");
 		CustomPropertySourceChain customPS = new CustomPropertySourceChain("CustomPropertySource");
@@ -84,7 +82,7 @@ public class ConjurValueProcessor implements BeanPostProcessor, InitializingBean
 
 	@Override
 	public void setEnvironment(Environment environment) {
-		// TODO Auto-generated method stub
+
 		if (environment instanceof ConfigurableEnvironment) {
 
 			this.environment = (ConfigurableEnvironment) environment;
@@ -97,76 +95,12 @@ public class ConjurValueProcessor implements BeanPostProcessor, InitializingBean
 
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		// TODO Auto-generated method stub
+
 		this.context = applicationContext;
 
 	}
 	
 	
-	public void initializeSysEnv(ConjurProperties conjurParam)
-	{
-		String authApiKey =null;
-			
-		String authTokenFile =conjurParam.getAuthTokenFile();
-		
-		LOGGER.info("Auth Token file path>>"+authTokenFile);
-			
-		if (authTokenFile != null) {
-			Map<String, String> conjurParameters = new HashMap<String, String>();
-			byte[] apiKey = null;
-			try (BufferedReader br = new BufferedReader(new FileReader(authTokenFile))){
-				StringBuilder sb = new StringBuilder();
-				String line = br.readLine();
 
-				while (line != null) {
-					sb.append(line);
-					sb.append(System.lineSeparator());
-					line = br.readLine();
-				}
-				apiKey =  sb.toString().getBytes();
-			} catch (Exception e1) {
-				e1.printStackTrace();
-
-			}
-
-			conjurParameters.put("CONJUR_AUTHN_API_KEY",new String(apiKey).trim());
-			conjurParameters.put("CONJUR_ACCOUNT", conjurParam.getAccount());
-			conjurParameters.put("CONJUR_APPLIANCE_URL", conjurParam.getApplianceUrl());
-			conjurParameters.put("CONJUR_AUTHN_LOGIN", conjurParam.getAuthnLogin());
-			conjurParameters.put("CONJUR_CERT_FILE", conjurParam.getCertFile());
-			conjurParameters.put("CONJUR_SSL_CERTIFICATE", conjurParam.getSslCertificate());
-			conjurParameters.put("CONJUR_AUTHN_TOKEN_FILE", conjurParam.getAuthTokenFile());
-			
-			apiKey=null;
-			try {
-			loadEnvironmentParameters(conjurParameters);
-			} catch (Exception e) {
-				
-				e.printStackTrace();
-				
-			}
-		
-		}
-	    else if (authApiKey == null && authTokenFile == null) {
-			 LOGGER.error(ConjurConstant.CONJUR_APIKEY_ERROR);
-
-		}
-	}
-	public  void loadEnvironmentParameters(Map<String, String> newenv)
-			throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
-		Class[] classes = Collections.class.getDeclaredClasses();
-		
-		Map<String, String> env = System.getenv();
-		for (Class cl : classes) {
-			if ("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
-				Field field = cl.getDeclaredField("m");
-				field.setAccessible(true);
-				Object obj = field.get(env);
-				Map<String, String> map = (Map<String, String>) obj;
-				map.putAll(newenv);
-				
-			}
-		}
-	}
 
 }
