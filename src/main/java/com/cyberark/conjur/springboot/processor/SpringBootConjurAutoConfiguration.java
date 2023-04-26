c90290e7325852064b063db1c3634ab6b2a4e9c2
Jaleela FaizurRahman <Jaleela.FaizurRahman@cyberark.com>
2023-04-26 18:08:17 +0530

    Included Reviewed Code

--- src/main/java/com/cyberark/conjur/springboot/processor/SpringBootConjurAutoConfiguration.java
+++ src/main/java/com/cyberark/conjur/springboot/processor/SpringBootConjurAutoConfiguration.java
@@ -1,19 +1,24 @@
 package com.cyberark.conjur.springboot.processor;
 
-import com.cyberark.conjur.sdk.endpoint.SecretsApi;
-import com.cyberark.conjur.springboot.core.env.AccessTokenProvider;
-import com.cyberark.conjur.springboot.core.env.ConjurConnectionManager;
-import com.cyberark.conjur.springboot.domain.ConjurProperties;
+import static com.cyberark.conjur.springboot.constant.ConjurConstant.CONJUR_PREFIX;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static com.cyberark.conjur.springboot.constant.ConjurConstant.CONJUR_PREFIX;
+import com.cyberark.conjur.sdk.endpoint.SecretsApi;
+import com.cyberark.conjur.springboot.core.env.AccessTokenProvider;
+import com.cyberark.conjur.springboot.core.env.ConjurConnectionManager;
+import com.cyberark.conjur.springboot.core.env.ConjurPropertySource;
+import com.cyberark.conjur.springboot.domain.ConjurProperties;
 
 @Configuration(proxyBeanMethods = false)
 public class SpringBootConjurAutoConfiguration {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringBootConjurAutoConfiguration.class);
 
 	@ConditionalOnMissingBean
 	@Bean
@@ -41,14 +46,24 @@ public class SpringBootConjurAutoConfiguration {
 
 	@ConditionalOnMissingBean
 	@Bean
-	AccessTokenProvider accessTokenProvider(){
+	AccessTokenProvider accessTokenProvider() {
 		return new AccessTokenProvider();
 	}
 
 	@ConditionalOnMissingBean
 	@ConfigurationProperties(prefix = CONJUR_PREFIX)
 	@Bean
-	ConjurProperties conjurProperties(){
+	ConjurProperties conjurProperties() {
 		return new ConjurProperties();
 	}
+
+	@ConditionalOnMissingBean(ConjurPropertySource.class)
+	@Bean
+	static ConjurCloudProcessor conjurCloudProcessor(SecretsApi secretsApi) {
+
+		LOGGER.info("Creating ConjurCloudProcessor instance");
+
+		return new ConjurCloudProcessor(secretsApi);
+	}
+
 }
---
+++ src/main/java/com/cyberark/conjur/springboot/service/DefaultPropertySourceChain.java
@@ -0,0 +1,55 @@
+package com.cyberark.conjur.springboot.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ * This class resolves the secret value at application load
+ * time from the conjur vault.
+ *
+ */
+
+public class DefaultPropertySourceChain extends PropertyProcessorChain {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(DefaultPropertySourceChain.class);
+
+	private PropertyProcessorChain chain;
+
+
+	public DefaultPropertySourceChain(String name) {
+
+		super("defaultPropertySource");
+		LOGGER.debug("Calling Defaultropertysource Chain");
+
+	}
+
+	@Override
+	public void setNextChain(PropertyProcessorChain nextChain) {
+
+		this.chain = nextChain;
+
+	}
+
+	@Override
+	public String[] getPropertyNames() {
+
+		return new String[0];
+	}
+
+	@Override
+	public Object getProperty(String name) {
+
+
+		Object value = null;
+
+		if (value == null) {
+			value =  this.chain.getProperty(name);
+
+		}
+        LOGGER.debug("property value : " + value);
+		return value;
+		
+	}
+
+}
